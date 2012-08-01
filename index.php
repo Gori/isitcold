4fcc22695aa6e544f70fd2ae7a07f623d67b3c1b
Oskar Sundberg <oskar@dread.se>
2012-08-01 13:32:11 +0200

    Sharing update
    New sharing functionality with correct weather icons for Facebook.

--- index.php
+++ index.php
@@ -3,12 +3,26 @@
 	<head>
 		<meta charset="UTF-8">
 		<title>Is It Cold?</title>
-		<link rel="stylesheet" href="css/isitcold.css" type="text/css">
+		<link rel="stylesheet" href="/css/isitcold.css" type="text/css">
 		<script type="text/javascript" src="//use.typekit.net/iyb1twf.js"></script>
 		<script type="text/javascript">try{Typekit.load();}catch(e){}</script>
 		<meta name="author" content="Kinda Like A Big Deal">
 		<meta name="description" content="The worlds cutest and smallest weather site.">
 		<meta name="keywords" content="weather,kindalikeabigdeal,isitcold">
+		<script>
+			<?php
+				if (isset($_GET['country']) && isset($_GET['city'])) { 
+					$country = $_GET['country'];
+					$city = $_GET['city'];
+				} else {
+					$country = "null";
+					$city = "null";
+				}
+			?>
+			var IsItCold = IsItCold || {};
+			IsItCold.country = "<?php echo $country ?>";
+			IsItCold.city = "<?php echo $city ?>";
+		</script>
 	</head>
 	<body>
 		<div id="container">
@@ -19,20 +33,23 @@
 						<div id='icontext'><div id='iconholder'>iconText</div></div>
 						<div class='headline' id='headline'>temperature</div>
 						<p class='textline' id='infotext'>description</p>
-						<p class='textline' id='sharetext'>Hejsan.</p>
+						<p class='textline' id='sharetext'>
+							Share this:<br>
+							<img id="twittershare" class="sharebutton" src="/img/twitter.png" width="48" height="48"/> <img id="faceshare" class="sharebutton" src="/img/facebook.png" width="48" height="48"/>
+						</p>
 					</div>
 					<div class="footer"><p class="social"><a href="https://twitter.com/share" class="twitter-share-button" data-url="http://www.isitcold.com" data-count="vertical">Tweet</a><script type="text/javascript" src="//platform.twitter.com/widgets.js"></script>
 <iframe src="//www.facebook.com/plugins/like.php?href=http%3A%2F%2Fwww.isitcold.com&amp;send=false&amp;layout=box_count&amp;width=55&amp;show_faces=false&amp;action=like&amp;colorscheme=light&amp;font&amp;height=62&amp;appId=144686638922410" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:55px; height:62px;" allowTransparency="true"></iframe>
-</p>Copyright © 2012 <a href="http://kindalikeabigdeal.com">Kinda Like A Big Deal</a> - <a href="about.html">Are we cold?</a>
+</p>Copyright © 2012 <a href="http://kindalikeabigdeal.com">Kinda Like A Big Deal</a> - <a href="/about.html">Are we cold?</a>
 </div>
 				</div>
 			</div>
 		</div>
 
-		<script src="js/vendor/jquery.js"></script>
-		<script src="js/vendor/TweenLite.js"></script>
-		<script src="js/vendor/plugins/CSSPlugin.js"></script>
-		<script src="js/vendor/plugins/EasePack.js"></script>
-		<script src="js/isitcold.js"></script>
+		<script src="/js/vendor/jquery.js"></script>
+		<script src="/js/vendor/TweenLite.js"></script>
+		<script src="/js/vendor/plugins/CSSPlugin.js"></script>
+		<script src="/js/vendor/plugins/EasePack.js"></script>
+		<script src="/js/isitcold.js"></script>
 	</body>
 </html>
--- js/isitcold.js
+++ js/isitcold.js
@@ -20,13 +20,23 @@
 	var latitude = 0;
 	var tempLine = "";
 	var shareMessage = "";
+	var shareIcon = "";
 	var currentCol = {r:128, g:128, b:128};
 	var targetCol = {r:0, g:0, b:0};
 
 	var autorun = function() {
 		console.log("autorun running()");
 
-		getGeo();
+		console.log("IS IT COLD: " + IsItCold.country);
+		console.log(IsItCold.city);
+
+		if (IsItCold.country != "null" && IsItCold.city != "null") {
+			city = IsItCold.city.toString();
+			country = IsItCold.country.toString();
+			getWeather();
+		} else {
+			getGeo();
+		}
 	};
 
 	var getGeo = function() {
@@ -46,14 +56,14 @@
 		city = parcity;
 		country = parname;
 
-		if (startPath) {
-			console.log("startPath found");
-			city = startPath.split(",")[0];
-			country = startPath.split(",")[1];
+		// if (startPath) {
+		// 	console.log("startPath found");
+		// 	city = startPath.split(",")[0];
+		// 	country = startPath.split(",")[1];
 
-			getWeather();
+		// 	getWeather();
 
-		} else if (city && country) {
+		if (city && country) {
 			city = encodeURIComponent(city);
 			country = encodeURIComponent(country);
 			country = country;
@@ -91,9 +101,9 @@
 	};
 
 	var getWeather = function() {
-		console.log("getWeather()");
-		var weatherURL = "ba-simple-proxy.php?url=http://www.google.com/ig/api?weather=" + city + "," + country;
-		//console.log("VÄDER: " + weatherURL);
+		console.log("getWeather() " + city + ", " + country);
+		var weatherURL = "/ba-simple-proxy.php?url=http://www.google.com/ig/api?weather=" + city + "," + country;
+		console.log("VÄDER: " + weatherURL);
 		var self = this;
 		$.ajax({
 			url: weatherURL,
@@ -169,6 +179,13 @@
 			localStorage.setObject("iscelsius", isCelsius);
 		}
 
+		if (IsItCold.country == "null" && IsItCold.city == "null") {
+			var stateData = {};
+			var stateTitle = "Hello";
+			var stateURL = country + "/" + city;
+			window.history.replaceState(stateData, stateTitle, stateURL);
+		}
+
 		setDescriptions();
 		render();
 	};
@@ -207,17 +224,40 @@
 	};
 
 	var getIcon = function() {
-		var icons = [{condition:"Partly Sunny", icon:"1", x:86, y:40},{condition:"Scattered Thunderstorms", icon:"x", x:86, y:40}, {condition:"Showers", icon:"e", x:86, y:-40},
-			{condition:"Rain Showers", icon:"e", x:86, y:-40}, {condition:"Scattered Showers", icon:"r", x:86, y:20}, {condition:"Rain and Snow", icon:"y", x:86, y:-50},
-			{condition:"Overcast", icon:"`", x:86, y:-30}, {condition:"Light Snow", icon:"o", x:86, y:-24}, {condition:"Freezing Drizzle", icon:"6", x:86, y:-24},
-			{condition:"Chance of Rain", icon:"=", x:86, y:-14}, {condition:"Sunny", icon:"v", x:68, y:8}, {condition:"Clear", icon:"v", x:68, y:8},
-			{condition:"Mostly Sunny", icon:"1", x:86, y:44}, {condition:"Partly Cloudy", icon:"1", x:86, y:44}, {condition:"Mostly Cloudy", icon:"`", x:86, y:6},
-			{condition:"Chance of Storm", icon:";", x:41, y:5}, {condition:"Rain", icon:"3", x:86, y:-53}, {condition:"Chance of Snow", icon:"\\", x:86, y:30},
-			{condition:"Cloudy", icon:"`", x:86, y:6}, {condition:"Mist", icon:"g", x:86, y:6}, {condition:"Storm", icon:"l", x:40, y:-56},
-			{condition:"Thunderstorm", icon:"z", x:86, y:-20}, {condition:"Chance of TStorm", icon:"x", x:86, y:20}, {condition:"Sleet", icon:"6", x:86, y:-30},
-			{condition:"Snow", icon:"*", x:75, y:9}, {condition:"Icy", icon:"*", x:75, y:9}, {condition:"Dust", icon:"h", x:86, y:50},{condition:"Fog", icon:"g", x:86, y:4},
-			{condition:"Smoke", icon:"s", x:86, y:-44}, {condition:"Haze", icon:"h", x:86, y:44}, {condition:"Flurries", icon:"q", x:86, y:32},
-			{condition:"Light rain", icon:"6", x:86, y:-20}, {condition:"Snow Showers", icon:"y", x:88, y:-62}, {condition:"Hail", icon:"*", x:75, y:9}];
+		var icons = [{condition:"Partly Sunny", share:"1.png", icon:"1", x:86, y:40},
+			{condition:"Scattered Thunderstorms", share:"x.png", icon:"x", x:86, y:40},
+			{condition:"Showers", share:"e.png", icon:"e", x:86, y:-40},
+			{condition:"Rain Showers", share:"e.png", icon:"e", x:86, y:-40},
+			{condition:"Scattered Showers", share:"r.png", icon:"r", x:86, y:20},
+			{condition:"Rain and Snow", share:"y.png", icon:"y", x:86, y:-50},
+			{condition:"Overcast", share:"over.png", icon:"`", x:86, y:-30},
+			{condition:"Light Snow", share:"o.png", icon:"o", x:86, y:-24},
+			{condition:"Freezing Drizzle", share:"6.png", icon:"6", x:86, y:-24},
+			{condition:"Chance of Rain", share:"chance.png", icon:"=", x:86, y:-14},
+			{condition:"Sunny", share:"v.png", icon:"v", x:68, y:8},
+			{condition:"Clear", share:"v.png", icon:"v", x:68, y:8},
+			{condition:"Mostly Sunny", share:"1.png", icon:"1", x:86, y:44},
+			{condition:"Partly Cloudy", share:"1.png", icon:"1", x:86, y:44},
+			{condition:"Mostly Cloudy", share:"over.png", icon:"`", x:86, y:6},
+			{condition:"Chance of Storm", share:"chancestorm.png", icon:";", x:41, y:5},
+			{condition:"Rain", share:"3.png", icon:"3", x:86, y:-53},
+			{condition:"Chance of Snow", share:"chancesnow.png", icon:"\\", x:86, y:30},
+			{condition:"Cloudy", share:"over.png", icon:"`", x:86, y:6},
+			{condition:"Mist", share:"g.png", icon:"g", x:86, y:6},
+			{condition:"Storm", share:"l.png", icon:"l", x:40, y:-56},
+			{condition:"Thunderstorm", share:"z.png", icon:"z", x:86, y:-20},
+			{condition:"Chance of TStorm", share:"x.png", icon:"x", x:86, y:20},
+			{condition:"Sleet", share:"6.png", icon:"6", x:86, y:-30},
+			{condition:"Snow", share:"snow.png", icon:"*", x:75, y:9},
+			{condition:"Icy", share:"snow.png", icon:"*", x:75, y:9},
+			{condition:"Dust", share:"h.png", icon:"h", x:86, y:50},
+			{condition:"Fog", share:"g.png", icon:"g", x:86, y:4},
+			{condition:"Smoke", share:"s.png", icon:"s", x:86, y:-44},
+			{condition:"Haze", share:"h.png", icon:"h", x:86, y:44},
+			{condition:"Flurries", share:"q.png", icon:"q", x:86, y:32},
+			{condition:"Light rain", share:"6.png", icon:"6", x:86, y:-20},
+			{condition:"Snow Showers", share:"y.png", icon:"y", x:88, y:-62},
+			{condition:"Hail", share:"snow.png", icon:"*", x:75, y:9}];
 
 		var i = arrayIndexOf(icons, function(obj) {
 			return obj.condition.toLowerCase() == condition.toLowerCase();
@@ -227,6 +267,7 @@
 			iconText = icons[i].icon;
 			iconX = icons[i].x;
 			iconY = icons[i].y;
+			shareIcon = icons[i].share;
 		} else {
 			console.log("!!!!! CONDITION NOT FOUND: " + condition);
 		}
@@ -247,15 +288,43 @@
 		$("#infotext").html(description);
 		$("#iconholder").html(iconText);
 
-		var tweetText = "Testar lite.";
-		var shareString = "Share this:<br><a href='https://twitter.com/share' data-url='" + window.location.href + "' class='twitter-share-button' data-text='" + shareMessage + "' data-count='none' data-hashtags='isitcold'>Tweet</a><script type='text/javascript' src='//platform.twitter.com/widgets.js'></script>";
+		//var tweetText = "Testar lite.";
+		//var shareString = "Share this:<br><a href='https://twitter.com/share' data-url='" + window.location.href + "' class='twitter-share-button' data-text='" + shareMessage + "' data-count='none' data-hashtags='isitcold'>Tweet</a><script type='text/javascript' src='//platform.twitter.com/widgets.js'></script>";
 
 		// <p class="sharers">Tweet this:<br><a href="https://twitter.com/share" data-url="' + urlString + '" class="twitter-share-button" data-text="' + tweetText + '" data-count="none" data-hashtags="isitold">Tweet</a><script type="text/javascript" src="//platform.twitter.com/widgets.js"></script></p>
 		// https://www.facebook.com/sharer/sharer.php?u=http://dread.se&t=Is%20It%20Cold?
 
-		$("#sharetext").html(shareString);
+		//$("#sharetext").html(shareString);
 
 		$('#tempswitch').bind('click', {}, switchTemp);
+
+		$('#faceshare').bind('click', {site:"facebook"}, sharePage);
+		$('#twittershare').bind('click', {site:"twitter"}, sharePage);
+	};
+
+	var sharePage = function(event) {
+		var shareName = "Is It Cold?";
+		var sharer = event.data.site;
+		var shareURL = encodeURIComponent(getShareUrl());
+		var shareTitle = shareMessage;
+		var shareImage = window.location.protocol + '//' + window.location.host + '/img/icons/' + shareIcon;
+		console.log("sharePage() " + sharer);
+
+		if (sharer == "facebook") {
+			var fName = encodeURIComponent(shareName);
+			var fCaption = encodeURIComponent(shareName);
+			var fDescription = encodeURIComponent(shareTitle);
+			//window.open("https://www.facebook.com/sharer/sharer.php?u=" + shareURL + "&t=" + shareTitle,"_blank");
+			var facebookShare = "https://www.facebook.com/dialog/feed?app_id=427587953959665&link=" + shareURL + "&picture=" + shareImage + "&name=" + fName + "&caption=" + fCaption + "&description=" + fDescription + "&redirect_uri=http://www.isitcold.com";
+	
+			window.open(facebookShare, "_blank");
+		} else if (sharer == "twitter") {
+			var shareLeft = (screen.width/2)-280;
+			var shareTop = (screen.height/2)-140;
+			window.open('http://twitter.com/share?url=' + encodeURIComponent(shareURL) + '&text=' + encodeURIComponent(shareTitle),"Facebook Share", "left=" + shareLeft + ",top=" + shareTop + ",width=560,height=280,scrollbars=no");
+		} else if (sharer == "pinterest") {
+			window.open('http://pinterest.com/pin/create/button/?url=' + shareURL + '&media=' + shareImage + '&description=' + shareTitle,"_blank");
+		}
 	};
 
 	var switchDay = function() {
@@ -305,6 +374,28 @@
 			var colVal = Math.round((Math.min(30, Math.max(-30, celsius)) + 30) / 3);
 			targetCol = {r:colRed[colVal], g:colGreen[colVal], b:colBlue[colVal]};
 		}
+	};
+
+	var getSharePath = function() {
+		var l = window.location;
+		var path;
+
+		if (l.hash && l.hash.match(/#?\//)) {
+			path = l.hash.replace('#', '');
+		} else {
+			path = l.pathname;
+		}
+
+		return path;
+	};
+
+	var getShareUrl = function() {
+		var l = window.location;
+		var url = l.protocol + '//' + l.host;
+
+		url += getSharePath();
+
+		return url;
 	};
 
 	var arrayIndexOf = function(a, fnc) {
